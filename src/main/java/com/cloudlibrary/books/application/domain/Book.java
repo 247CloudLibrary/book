6cef4a3ff371c60cf5119e7f2a61461b6d5cd043
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-04-04 19:19:17 +0900

    [create] controller와 service 구조

---
+++ src/main/java/com/cloudlibrary/books/application/domain/Book.java
@@ -0,0 +1,12 @@
+package com.cloudlibrary.books.application.domain;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
+public class Book {
+}
---
+++ src/main/java/com/cloudlibrary/books/ui/controller/BookController.java
@@ -0,0 +1,12 @@
+package com.cloudlibrary.books.ui.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/v1/books")
+public class BookController {
+}
