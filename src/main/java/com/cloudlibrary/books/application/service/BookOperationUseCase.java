22f3efc9680029f1558ef359ce49cde773570b38
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-04-15 05:50:32 +0900

    [update] add Book OperationUseCase

--- src/main/java/com/cloudlibrary/books/application/service/BookOperationUseCase.java
+++ src/main/java/com/cloudlibrary/books/application/service/BookOperationUseCase.java
@@ -1,4 +1,73 @@
 package com.cloudlibrary.books.application.service;
 
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
 public interface BookOperationUseCase {
+    void createBook(BookCreateCommand command);
+    Long updateBook(BookUpdateCommand command);
+    void deleteBook(BookDeleteCommand command);
+
+    @EqualsAndHashCode(callSuper = false)
+    @Builder
+    @Getter
+    @ToString
+    class BookCreateCommand {
+        private String rid;
+        private String isbn;
+        private String title;
+        private String thumbNailImage;
+        private String coverImage;
+        private String author;
+        private String translator;
+        private String contents;
+        private String publisher;
+        private LocalDate publishDate;
+        private String bookType;
+        private String genre;
+        private String barcode;
+        private String rfid;
+        private String bookStatus;
+        private Long categoryId;
+        private String libraryName;
+
+    }
+
+    @EqualsAndHashCode(callSuper = false)
+    @Builder
+    @Getter
+    @ToString
+    class BookUpdateCommand{
+        private Long id;
+        private String rid;
+        private String isbn;
+        private String title;
+        private String thumbNailImage;
+        private String coverImage;
+        private String author;
+        private String translator;
+        private String contents;
+        private String publisher;
+        private LocalDate publishDate;
+        private String bookType;
+        private String genre;
+        private String barcode;
+        private String rfid;
+        private String bookStatus;
+        private Long categoryId;
+        private String libraryName;
+
+    }
+
+    @EqualsAndHashCode(callSuper = false)
+    @Builder
+    @Getter
+    @ToString
+    class BookDeleteCommand{
+        private Long id;
+    }
 }
