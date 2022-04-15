164ce1774a698c19dadee42984f8a01f6f0c725b
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-04-15 21:57:55 +0900

    [update] add Book Genre

---
+++ src/main/java/com/cloudlibrary/books/infrastructure/persistence/mysql/entity/BookGenreEnum.java
@@ -0,0 +1,19 @@
+package com.cloudlibrary.books.infrastructure.persistence.mysql.entity;
+
+/**
+ * 액션: ACTION
+ * SF/판타지: SF_AND_FANTASY
+ * 코미디: COMEDY
+ * 로맨스: ROMANCE
+ * 공포/스릴러: HORROR_AND_THRILLER
+ * 기타: OTHERS
+ */
+public enum BookGenreEnum {
+    ACTION
+    ,SF_AND_FANTASY
+    ,COMEDY
+    ,ROMANCE
+    ,HORROR_AND_THRILLER
+    ,OTHERS
+
+}
