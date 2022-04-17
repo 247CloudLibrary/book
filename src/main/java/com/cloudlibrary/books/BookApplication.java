505ffc9868e5cd0ebc317c590dd61ea667c46505
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-04-17 23:55:42 +0900

    [update] add Feign Client, JPA Auditing 어노테이션

--- src/main/java/com/cloudlibrary/books/BookApplication.java
+++ src/main/java/com/cloudlibrary/books/BookApplication.java
@@ -2,8 +2,13 @@ package com.cloudlibrary.books;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+
+@EnableFeignClients
+@EnableJpaAuditing
 @EnableWebMvc
 @SpringBootApplication
 public class BookApplication {
