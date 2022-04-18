60f086f5aefa597e01317dc18024c14557e0b6b8
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-04-19 02:03:49 +0900

    [update] fix DB Configuration

--- src/main/java/com/cloudlibrary/books/infrastructure/configuration/DatabaseConfiguration.java
+++ src/main/java/com/cloudlibrary/books/infrastructure/configuration/DatabaseConfiguration.java
@@ -1,6 +1,7 @@
 package com.cloudlibrary.books.infrastructure.configuration;
 
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +14,8 @@ import javax.sql.DataSource;
 public class DatabaseConfiguration {
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource(){ return DataSourceBuilder.create().build(); }
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public DataSource dataSource(){ return DataSourceBuilder.create().type(HikariDataSource.class).build(); }
 
 
 }
