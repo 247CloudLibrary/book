82543996941c58a213033ab257f91c39782cea31
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-04-19 14:59:55 +0900

    [update] 날짜 출력 형식 수정,publishDate 추가

--- src/main/java/com/cloudlibrary/books/ui/view/book/BookCompositeView.java
+++ src/main/java/com/cloudlibrary/books/ui/view/book/BookCompositeView.java
@@ -8,9 +8,8 @@ import lombok.Getter;
 import lombok.ToString;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
-@Builder //추후 삭제
+
 @Getter
 @ToString
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -36,7 +35,7 @@ public class BookCompositeView {
     @ApiModelProperty(value = "출판사")
     private String publisher;
     @ApiModelProperty(value="출판일자")
-    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMdd")
     private LocalDate publishDate;
     @ApiModelProperty(value="자료 종류")
     private String type;
