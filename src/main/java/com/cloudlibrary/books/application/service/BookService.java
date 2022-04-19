c4ccb6e47516961f7326ee55d0454c7748d4c267
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-04-19 14:21:06 +0900

    [update] 도서 목록 조회 controller, service

--- src/main/java/com/cloudlibrary/books/application/service/BookService.java
+++ src/main/java/com/cloudlibrary/books/application/service/BookService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -71,8 +72,11 @@ public class BookService implements BookReadUseCase,BookOperationUseCase {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<FindBookResult> getBookAllList() {
-        return null;
+        List<Book> findAllBook = bookEntityRepository.findAll().stream().map(BookEntity::toBook).collect(Collectors.toList());
+
+        return findAllBook.stream().map(FindBookResult::findByBook).collect(Collectors.toList());
     }
 
     @Override
